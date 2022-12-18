8d8e074d38b3be53644b96c53b17102cc3ab521a
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-12-18 11:54:40 -0700

    Update RecipeCreate.js

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,43 +1,92 @@
 import React, { useState } from "react";
 
+
 function RecipeCreate({createRecipe}) {
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  const [newRecipe, setNewRecipe] = useState("")
-const handleNewRecipe = (event) => setNewRecipe(event.target.value)
+   const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "", 
+    preparation: "",
+  }
+  const [newRecipe, setNewRecipe] = useState({...initialFormState})
+const handleNewRecipe = ({target}) => {
+  setNewRecipe((previous) => ({
+    ...previous,
+    [target.name]: target.value
+  }))
+} 
 const handleSubmit = (event) => {
 event.preventDefault()
-setNewRecipe("")
+  createRecipe(newRecipe)
+setNewRecipe({...initialFormState})
 }
 
   return (
     <form name="create" onSubmit = {handleSubmit}>
       <table>
         <tbody>
-        <label htmlFor = "new recipe">New Recipe</label>
           <tr>
-          
-            <div className = "inputValues">
-              <textarea 
-              id="content"
-              name="content"
+          <td>
+            <input 
+              id = "name"
+              type = "text"
+              name = "name"
               required = {true}
-              rows = {1}
-              columns = {3}
               onChange = {handleNewRecipe}
-              value = {newRecipe}
+             value = {newRecipe.name}
+             placeholder = "name"
               />
-               <input id = "urlcontent"
+            </td>
+          <td>
+            <input id = "cuisine"
+              type = "text"
+              name = "cuisine"
+              required = {true}
+              onChange = {handleNewRecipe}
+             value = {newRecipe.cuisine}
+             placeholder = "cuisine"
+              />
+            </td>
+            <td>
+               <input id = "photo"
               type = "url"
-              name = "content"
+              name = "photo"
               required = {true}
               onChange = {handleNewRecipe}
-              value = {newRecipe}
-              
+             value = {newRecipe.photo}
+             placeholder = "photo"
               />
-            </div>
+            </td>
+              <td>
+              <textarea 
+              id="ingredients"
+              name="ingredients"
+              required = {true}
+              type = "text"
+               rows = {3}
+              onChange = {handleNewRecipe}
+              value = {newRecipe.ingredients}
+              placeholder = "ingredients"
+                />
+            </td>
+             <td htmlFor = "preparation">
+              <textarea 
+              id="preparation"
+              name="preparation"
+              required = {true}
+              type = "text"
+              rows = {3}
+            onChange = {handleNewRecipe}
+              value = {newRecipe.preparation}
+              placeholder = "preparation"
+                />
+            </td>
+          
             <td>
               <button type="submit">Create</button>
             </td>
