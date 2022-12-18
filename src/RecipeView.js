f82be8d3010864a20b57aa21d930a980f71f61d2
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-12-18 11:55:41 -0700

    Update RecipeView.js

--- src/RecipeView.js
+++ src/RecipeView.js
@@ -1,17 +1,19 @@
 import React from "react"
 
+
 function RecipeView({findRecipe, deleteRecipe}) {
-    
+
     const row = 
-        <tr key = {findRecipe.name}>
+        <tr key = {findRecipe}>
             
-            <td>{findRecipe.name}</td>
-            <td>{findRecipe.cuisine}</td>
-            <td>{findRecipe.photo}</td>
-            <td>{findRecipe.ingredients}</td>
-            <td>{findRecipe.preparation}</td>
+            <td id = "name">{findRecipe.name}</td>
+            <td id = "cuisine">{findRecipe.cuisine}</td>
+            <td id = "photo"><img alt="new" src = {findRecipe.photo} /></td>
+            <td id = "content_td"><p>{findRecipe.ingredients}</p></td>
+            <td id = "content_td"><p>{findRecipe.preparation}</p></td>
             <td><button name = "delete" onClick = {() => deleteRecipe(findRecipe.name)}>Delete</button></td>
         </tr> 
+        
         
     return (
        <>
