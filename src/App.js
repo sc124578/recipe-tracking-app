6179b44903b289ac2e7eb9b4d4b44f4d23d283f0
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-12-20 23:04:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,31 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
-  const createRecipe = (recipe) =>
-setRecipes((currentRecipe) => [
-recipe,
-...currentRecipe
-])
-
-const deleteRecipe = (deleteR) => 
-
-setRecipes((currentRecipe) => 
-currentRecipe.filter((recipe) => recipe.name !== deleteR)
-)
-
-
+  
+//   setRecipes((prev) => [...prev, newRecipe])
+  const addRecipes = (data) => {
+    setRecipes([...recipes, data])
+  }
+  
+  // const newRecipe = {name: sdfkljasdf, cui}
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // index !== length -1?
+  const deleteRecipe = (recipeToDelete) => {
+    setRecipes((currentRecipes) => currentRecipes.filter((recipe, index) => index !== recipeToDelete))
+  }
+  
+  
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
 
+  
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList recipes = {recipes}
-      deleteRecipe = {deleteRecipe}
-      /> 
-      <RecipeCreate createRecipe = {createRecipe}/>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipes={addRecipes} />
     </div>
-    
   );
 }
 
